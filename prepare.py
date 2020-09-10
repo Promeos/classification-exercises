eb3315915e63ddae76314e52d5b6b0ab46b689d4
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-09-10 07:51:36 -0500

    added comments to functions and comment to notebook.

--- prepare.py
+++ prepare.py
@@ -9,11 +9,16 @@ def prep_iris(df):
     for exploratory analysis.
     type(df) >>> pandas.core.frame.DataFrame
     '''
+    # Drop columns of redundant data or 'index-like'/ordinal row.
     df.drop(columns=['species_id', 'measurement_id'], inplace=True)
+    
+    # Rename species_name to be concise.
     df.rename(columns={'species_name': 'species'}, inplace=True)
     
+    # Create dummy variables for our targets - 0 0 represents 'species_setosa'
     encoded_species = pd.get_dummies(df.species, prefix='species', drop_first=True)
     
+    # Add the encoded target names as columns to the dataframe.
     df = pd.concat([df, encoded_species], axis=1)
     
     return df
@@ -25,15 +30,24 @@ def prep_titanic(df):
     for exploratory analysis.
     type(df) >>> pandas.core.frame.DataFrame
     '''
-    
+    # Drop missing values in the embarked column.
+    # This removes missing values in embark_town as well.
     df.dropna(how='any', subset=['embarked'], inplace=True)
+    
+    # Throw the deck overboard because there are too many missing values.
     df.drop(columns='deck', inplace=True)
     
+    # Create dummy variables for our targets. 0 0 represents 'embarked_c'
     encoded_embarked = pd.get_dummies(df.embarked, prefix='embarked', drop_first=True)
     
+    # Scale numerical columns using MinMaxScalar()
     scaler = MinMaxScaler()
+    
+    # Use `.transform_fit` on the scalar object to fit and transform the data.
+    # Assign directly to 'age' and 'fare' columns.
     df[['age', 'fare']] = scaler.fit_transform(df[['age', 'fare']])
     
+    # Add the encoded target names as columns to the dataframe.
     df = pd.concat([df, encoded_embarked], axis=1)
     
     return df
